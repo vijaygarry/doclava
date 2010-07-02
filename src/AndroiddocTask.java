cb1aea330a30ea9d5eebbbdf196a2f74768910e1
bjdodson <bjdodson@google.com>
2010-07-02 23:46:10 +0000

    Style updates

--- src/AndroiddocTask.java
+++ src/AndroiddocTask.java
@@ -1,85 +1,90 @@
 
-
-import com.sun.javadoc.LanguageVersion;
-
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AndroiddocTask extends Task {
-    
-    Arguments args = null;
-    
-    @Override
-    public void execute() {
-        if (args == null) {
-            throw new BuildException("No command specified.");
-        }
-        
-        /*
-        String[] argz = args.getArguments();
-        for (int i=0;i<argz.length;i++) {
-            System.out.println(argz[i]);
-        }
-        */
-        try {
-          
-          // TODO: fork a process to run java on DroidDoc.main
-          com.sun.tools.javadoc.Main.execute(args.getArguments());
-          
-            
-        } catch (Exception e) {
-            throw new BuildException("Error generating documentation.",e);
-        }
+public final class AndroiddocTask extends Task {
+
+  Arguments args;
+
+  @Override
+  public void execute() {
+    if (args == null) {
+      throw new BuildException("No command specified.");
     }
 
-    // Consumes <arguments/> build.xml element
-    public Arguments createArguments() {
-        args = new Arguments();
-        return args;
+    try {
+
+      // TODO: fork a process to run java on DroidDoc.main
+      com.sun.tools.javadoc.Main.execute(args.getArgumentsList().toArray(new String[] {}));
+
+
+    } catch (Exception e) {
+      throw new BuildException("Error generating documentation.", e);
     }
-    
-    public class Arguments {
-        private String text;
-        public Arguments() {}
-        
-        public void addText(String text) {
-           this.text=getProject().replaceProperties(text);
+  }
+
+  /**
+   * Consumes <arguments/> build.xml element.
+   * 
+   */
+  public Arguments createArguments() {
+    args = new Arguments();
+    return args;
+  }
+
+  public class Arguments {
+    private String text;
+
+    public Arguments() {
+    }
+
+    public void addText(String text) {
+      this.text = getProject().replaceProperties(text);
+    }
+
+    /**
+     * Returns a list of arguments. Arguments are whitespace-separated character
+     * sequences. If an argument starts with a double quotation mark, then the
+     * argument consists of all characters (including whitespace) preceding the
+     * closing quotation mark.
+     */
+    public List<String> getArgumentsList() {
+      if (text == null) {
+        return null;
+      }
+
+      List<String> parts = new ArrayList<String>();
+      int currentPosition = 0, length = text.length();
+      while (currentPosition < length) {
+
+        int openQuotePosition = text.indexOf('"', currentPosition);
+        int closeQuotePosition;
+        if (openQuotePosition == -1) {
+          openQuotePosition = closeQuotePosition = length;
+        } else {
+          closeQuotePosition = text.indexOf('"', openQuotePosition + 1);
+          if (closeQuotePosition == -1) {
+            throw new BuildException("Unterminated quote in arguments.");
+          }
         }
-        
-        public String[] getArguments() {
-            if (text == null) return null;
-            List<String>parts = new ArrayList<String>();
-            int cur = 0, length = text.length();
-            while (cur < length) {
-                
-                int open_quote_pos = text.indexOf('"', cur);
-                int close_quote_pos;
-                if (open_quote_pos == -1) {
-                    open_quote_pos = close_quote_pos = length;
-                } else {
-                    close_quote_pos = text.indexOf('"', open_quote_pos+1);
-                    if (close_quote_pos == -1) {
-                        throw new BuildException("Unterminated quote in command.");
-                    }
-                }
-                
-                String[] cmds = text.substring(cur, open_quote_pos).split("\\s+");
-                for (int i = 0; i < cmds.length; i++) {
-                    if (cmds[i].length() > 0) {
-                        parts.add(cmds[i]);
-                    }
-                }
-                if (open_quote_pos < close_quote_pos) {
-                    parts.add(text.substring(open_quote_pos+1,close_quote_pos));
-                }
-                
-                cur = close_quote_pos+1;
-            }
-            
-            return parts.toArray(new String[]{});
-        } 
+
+        String[] cmds = text.substring(currentPosition, openQuotePosition).split("\\s+");
+        for (int i = 0; i < cmds.length; i++) {
+          if (cmds[i].length() > 0) {
+            parts.add(cmds[i]);
+          }
+        }
+        if (openQuotePosition < closeQuotePosition) {
+          parts.add(text.substring(openQuotePosition + 1, closeQuotePosition));
+        }
+
+        currentPosition = closeQuotePosition + 1;
+      }
+
+      return parts;
     }
+  }
 }
