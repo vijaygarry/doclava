37861a46384a391e4d66fa65d4dc3913f2b82175
bjdodson <bjdodson@google.com>
2010-07-13 23:31:45 +0000

    Updated copyright headers on all .java files

--- src/com/google/doclava/TagInfo.java
+++ src/com/google/doclava/TagInfo.java
@@ -1,21 +1,22 @@
-package com.google.doclava;
 /*
- * Copyright (C) 2008 The Android Open Source Project
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * 
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-import com.google.clearsilver.jsilver.data.Data;
+package com.google.doclava;
 
-import org.clearsilver.CS;
+import com.google.clearsilver.jsilver.data.Data;
 
 public class TagInfo {
   private String mName;
