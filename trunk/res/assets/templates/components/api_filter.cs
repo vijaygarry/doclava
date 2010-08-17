5647045e53d6ad7cfd985dd04b3daccfcd368a0d
bjdodson@google.com <bjdodson@google.com@a55b3ece-3bc4-011c-dab5-9a01923c4fb2>
2010-08-17 00:03:54 +0000

    Fix for tree navigation and api filtering

--- trunk/res/assets/templates/components/api_filter.cs
+++ trunk/res/assets/templates/components/api_filter.cs
@@ -1,5 +1,6 @@
 <?cs # The default API filter selector that goes in the header ?><?cs
-def:default_api_filter() ?>
+def:default_api_filter() ?><?cs
+  if:reference.apilevels ?>
   <div id="api-level-toggle">
     <input type="checkbox" id="apiLevelCheckbox" onclick="toggleApiLevelSelector(this)" />
     <label for="apiLevelCheckbox" class="disabled">Filter by API Level: </label>
@@ -8,12 +9,14 @@ def:default_api_filter() ?>
     </select>
   </div>
   <script>
-    var SINCE_DATA = [ <?cs 
+   var SINCE_DATA = [ <?cs 
       each:since = since ?>'<?cs 
         var:since.name ?>'<?cs 
         if:!last(since) ?>, <?cs /if ?><?cs
       /each 
     ?> ];
     buildApiLevelSelector();
+    addLoadEvent(changeApiLevel);
   </script>
+<?cs /if ?>
 <?cs /def ?>
